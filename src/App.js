258b4beae69025a1e3e0b9fba56df0e65c6ec80a
RoninXYZ <xyzronin97@gmail.com>
2022-08-26 10:13:32 +0700

    pake chakra ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import { IconButton } from "@chakra-ui/button";
+import { useColorMode } from "@chakra-ui/color-mode";
+import { Flex, VStack, Heading, Spacer } from "@chakra-ui/layout";
+import { FaSun, FaMoon, FaInstagram, FaGithub, FaLinkedin } from 'react-icons/fa'
+import Header from "./components/Headers";
+import Profile from "./components/Profile";
+import Social from "./components/Social";
 
 function App() {
+
+  const { colorMode, toggleColorMode } = useColorMode();
+  const isDark = colorMode === "dark";
+
   return (
-    <div>App</div>
-  )
+    <VStack p={5}>
+      <Flex w="100%">
+        <Heading
+          ml="8" size="md" fontWeight='semibold' color="cyan.400">imthepk</Heading>
+
+        <Spacer></Spacer>
+        <IconButton icon={<FaLinkedin />} isRound='true' onClick={toggleColorMode}></IconButton>
+        <IconButton ml={2} icon={<FaInstagram />} isRound='true' onClick={toggleColorMode}></IconButton>
+        <IconButton ml={2} icon={<FaGithub />} isRound='true' onClick={toggleColorMode}></IconButton>
+        <IconButton ml={8} icon={isDark ? <FaSun /> : <FaMoon />} isRound='true' onClick={toggleColorMode}></IconButton>
+      </Flex>
+      <Header></Header>
+      <Social></Social>
+      <Profile></Profile>
+    </VStack>
+  );
 }
 
-export default App
+export default App;
